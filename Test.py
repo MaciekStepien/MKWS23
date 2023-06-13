f7180f1b431d415c6db240926f48a1aaacc5f579
MaciekStepien <57720040+MaciekStepien@users.noreply.github.com>
2023-06-13 17:54:02 +0200

    Good_T_P_multicore
    All seems to be functioning well with those simple calculations. Next step is to extend the code with more cases

--- Test.py
+++ Test.py
@@ -1,51 +1,68 @@
+#"GOOD_T_P_Multicore"
 import cantera as ct
 import numpy as np
 import matplotlib.pyplot as plt
 from sdtoolbox.postshock import CJspeed
+from multiprocessing import Pool, cpu_count
 
-# Initial conditions
-P1 = 100000
-Pmax = 1000000
-T1 = 300
-Tmax = 1000
-p_steps = 10
-T_steps = 8
-q = 'CH4:1.0 O2:2.0 N2:7.52'  # Composition of the mixture
+# Define a function to calculate CJ speed for a given set of parameters
+def calculate_cj_speed(args):
+    p, T, q, mech = args
+    gas = ct.Solution(mech)
+    gas.TPX = T, p, q
+    cj_speed = CJspeed(p, T, q, mech)
+    return cj_speed
 
-mech = 'gri30.cti'
-gas = ct.Solution(mech)
+# Define input parameters
+P1 = 100000  # Initial pressure [Pa]
+Pmax = 1000000  # Maximum pressure [Pa]
+T1 = 300  # Initial temperature [K]
+Tmax = 1000  # Maximum temperature [K]
+p_steps = 10  # Number of pressure steps
+T_steps = 8  # Number of temperature steps
+q = 'CH4:1.0 O2:2.0 N2:7.52'  # Composition
+mech = 'gri30.cti'  # Mechanism file
 
-P = np.linspace(P1, Pmax, p_steps)  # Pressure range
-pbar = P / 100000  # Pressure in bars
+# Generate the list of arguments for the multiprocessing calculation
+args = [(p, T, q, mech) for p in np.linspace(P1, Pmax, p_steps) for T in np.linspace(T1, Tmax, T_steps)]
 
-T_array = np.linspace(T1, Tmax, T_steps)  # Temperature range
-cj_speed = np.zeros((p_steps, T_steps))  # Array to store CJ speeds
+if __name__ == '__main__':
+    # Create a multiprocessing pool with the number of available CPU cores
+    pool = Pool(processes=cpu_count())
 
-# Loop over pressure and temperature values
-for i, p in enumerate(P):
-    for j, T in enumerate(T_array):
-        gas.TPX = T, p, q  # Set the gas state
-        cj_speed[i, j] = CJspeed(p, T, q, mech)  # Compute CJ speed
+    # Use multiprocessing to calculate CJ speeds for each set of parameters
+    cj_speed = np.array(pool.map(calculate_cj_speed, args)).reshape((p_steps, T_steps))
 
-print('CJ computation for ' + mech + ' with composition ' + q)
-print('CJ speed:\n', np.round(cj_speed, 1), '(m/s)')
+    # Close the pool of processes
+    pool.close()
+    pool.join()
 
-# Plot CJ detonation speed vs pressure
-plt.figure(figsize=(20, 10))
-for i in range(T_steps // 2):
-    plt.plot(pbar, cj_speed[:, 2 * i], label='T = %.0f K' % T_array[2 * i])
-plt.xlabel('Pressure [bar]')
-plt.ylabel('CJ detonation speed [m/s]')
-plt.title('CJ Detonation Speed vs Pressure')
-plt.legend()
-plt.show()
+    # Output CJ speeds
+    print('CJ computation for ' + mech + ' with composition ' + q)
+    print('CJ speed:\n', np.round(cj_speed, 1), '(m/s)')
 
-# Plot CJ detonation speed vs temperature
-plt.figure(figsize=(20, 10))
-for i in range(p_steps // 2):
-    plt.plot(T_array, cj_speed[2 * i, :], label='p = %.1f bar' % pbar[2 * i])
-plt.xlabel('Temperature [K]')
-plt.ylabel('CJ detonation speed [m/s]')
-plt.title('CJ Detonation Speed vs Temperature')
-plt.legend()
-plt.show()
+    # Set the font for the plots
+    font = {'family': 'DejaVu Sans', 'weight': 'normal', 'size': 18}
+    plt.rc('font', **font)
+
+    # Plot CJ detonation speed vs. pressure
+    plt.figure(figsize=(20, 10))
+    for i in range(T_steps // 2):
+        T_array = np.linspace(T1, Tmax, T_steps)
+        plt.plot(np.linspace(P1, Pmax, p_steps), cj_speed[:, 2 * i], label='T = %.0f K' % T_array[2 * i])
+    plt.xlabel('Pressure [Pa]')
+    plt.ylabel('CJ detonation speed [m/s]')
+    plt.title('CJ Detonation Speed vs Pressure')
+    plt.legend()
+    plt.show()
+
+    # Plot CJ detonation speed vs. temperature
+    plt.figure(figsize=(20, 10))
+    for i in range(p_steps // 2):
+        pbar = np.linspace(P1, Pmax, p_steps)
+        plt.plot(T_array, cj_speed[2 * i, :], label='p = %.1f bar' % pbar[2 * i])
+    plt.xlabel('Temperature [K]')
+    plt.ylabel('CJ detonation speed [m/s]')
+    plt.title('CJ Detonation Speed vs Temperature')
+    plt.legend()
+    plt.show()
